5f68f80719acece2637577b7e28efebd2051fbf5
Yann Nicolas <yann.nicolas@thalesgroup.com>
2013-12-03 18:42:48 -0600

    Implemented optimized solution for problem 1 using a separate method to do the calculation of the sum of the multiples of a number.

--- Problem1/src/main/java/net/projecteuler/yannart/problem1/MultiplesOf3And5Main.java
+++ Problem1/src/main/java/net/projecteuler/yannart/problem1/MultiplesOf3And5Main.java
@@ -14,6 +14,8 @@ package net.projecteuler.yannart.problem1;
 public class MultiplesOf3And5Main {
 
 	/**
+	 * Optimal solution.
+	 * 
 	 * Do the sum of the numbers multiples of 3 or 5 below the limit number.
 	 * 
 	 * @param limitNum
@@ -21,6 +23,47 @@ public class MultiplesOf3And5Main {
 	 * @return sum
 	 */
 	public static int findSumMultiples3And5(int limitNum) {
+
+		return sumMultiples(3, limitNum - 1) + sumMultiples(5, limitNum - 1)
+				- sumMultiples(3 * 5, limitNum - 1);
+	}
+
+	/**
+	 * Calculates the sum of the multiples of <code>coef</code> from 0 to limit
+	 * excluded.
+	 * 
+	 * @param coef
+	 *            coefficient to find its multiples.
+	 * @param limit
+	 *            maximum excluded number for the sum.
+	 * @return sum of the multiples of <code>coef</code> from 0 to limit
+	 *         included
+	 */
+	public static int sumMultiples(int coef, int limit) {
+
+		/*
+		 * The sum of multiples of 5 with a limit of 22 for eg: 5 + 10 + 15 + 20
+		 * => 5 * (1 + 2 + 3 + 4) => 5 * ((1 + 4) / 2) * 4
+		 * 
+		 * If M is the multiple coef and L the limit number: M ((1 + L/M) / 2) *
+		 * L/M
+		 */
+
+		int n = limit / coef;
+
+		return coef * (int) ((1 + n) / 2d * n);
+	}
+
+	/**
+	 * Obvious not optimized solution.
+	 * 
+	 * Do the sum of the numbers multiples of 3 or 5 below the limit number.
+	 * 
+	 * @param limitNum
+	 *            limit number.
+	 * @return sum
+	 */
+	public static int findSumMultiples3And5Obvious(int limitNum) {
 
 		int sum = 0;
 
--- Problem1/src/test/java/net/projecteuler/yannart/problem1/MultiplesOf3And5MainTest.java
+++ Problem1/src/test/java/net/projecteuler/yannart/problem1/MultiplesOf3And5MainTest.java
@@ -9,29 +9,61 @@ import org.junit.Test;
 
 /**
  * @author T0115541
- *
+ * 
  */
 public class MultiplesOf3And5MainTest {
 
-	
 	/**
-	 * Test method for {@link net.projecteuler.yannart.problem1.MultiplesOf3And5Main#findSumMultiples3And5(int)}.
+	 * Test method for
+	 * {@link net.projecteuler.yannart.problem1.MultiplesOf3And5Main#findSumMultiples3And5(int)}
+	 * .
 	 */
 	@Test
 	public void testFindSumMultiples3And5Example() {
-		
+
 		// Example from the problem description, the numbers are below 10
 		assertEquals(23, MultiplesOf3And5Main.findSumMultiples3And5(10));
 	}
-	
+
 	/**
-	 * Test method for {@link net.projecteuler.yannart.problem1.MultiplesOf3And5Main#findSumMultiples3And5(int)}.
+	 * Test method for
+	 * {@link net.projecteuler.yannart.problem1.MultiplesOf3And5Main#findSumMultiples3And5(int)}
+	 * .
 	 */
 	@Test
 	public void testFindSumMultiples3And5() {
-		
+
 		// Problem to solve, the numbers are below 1000
 		assertEquals(233168, MultiplesOf3And5Main.findSumMultiples3And5(1000));
 	}
 
+	/**
+	 * Test method for
+	 * {@link net.projecteuler.yannart.problem1.MultiplesOf3And5Main#sumMultiples(int, int)}
+	 * .
+	 */
+	@Test
+	public void testSumMultiples() {
+
+		// Test to add the multiples of 1 from 1 to 10
+		assertEquals(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10,
+				MultiplesOf3And5Main.sumMultiples(1, 10));
+
+		// Test to add the multiples of 2 from 1 to 20
+		assertEquals(2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20,
+				MultiplesOf3And5Main.sumMultiples(2, 20));
+
+		// Test to add the multiples of 2 from 1 to 21
+		assertEquals(2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20,
+				MultiplesOf3And5Main.sumMultiples(2, 21));
+
+		// Test to add the multiples of 5 from 1 to 9
+		assertEquals(5, MultiplesOf3And5Main.sumMultiples(5, 9));
+
+		// Test to add the multiples of 3 from 1 to 9
+		assertEquals(3 + 6 + 9, MultiplesOf3And5Main.sumMultiples(3, 9));
+
+		// Test to add the multiples of 15 from 1 to 9
+		assertEquals(0, MultiplesOf3And5Main.sumMultiples(15, 9));
+	}
 }
